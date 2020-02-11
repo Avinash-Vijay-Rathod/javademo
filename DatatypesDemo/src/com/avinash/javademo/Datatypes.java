11377c28b5287f1e431d0b02a3f7313511f02cac
avinash_rathod <avinash.rathod@perficient.com>
2020-02-11 17:59:37 +0530

    data types demo

---
+++ DatatypesDemo/src/com/avinash/javademo/Datatypes.java
@@ -0,0 +1,18 @@
+package com.avinash.javademo;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Range of Integers is "+Integer.MIN_VALUE+" to "+Integer.MAX_VALUE);
+		System.out.println("Range of Bytes is "+Byte.MIN_VALUE+" to "+Byte.MAX_VALUE);
+		System.out.println("Range of Short is "+Short.MIN_VALUE+" to "+Short.MAX_VALUE);
+		System.out.println("Range of Long is "+Long.MIN_VALUE+" to "+Long.MAX_VALUE);
+		System.out.println("Range of Float is "+Float.MIN_VALUE+" to "+Float.MAX_VALUE);
+		System.out.println("Range of Double is "+Double.MIN_VALUE+" to "+Double.MAX_VALUE);
+		
+		// min and max values for character are not defined in wrapper class
+		System.out.println("Range of Character is "+Character.MIN_VALUE+" to "+Character.MAX_VALUE);
+	}
+
+}
