63aba59cd8236bb4cdf6682304035fb3d2012481
avinash_rathod <avinash.rathod@perficient.com>
2020-02-20 17:50:05 +0530

    udemi exercise

---
+++ UdemyCodingExercise/src/com/speedconverter/Main.java
@@ -0,0 +1,11 @@
+package com.speedconverter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new AreaCalculator();
+		System.out.println(AreaCalculator.area(-5.0, -4.0));
+	}
+
+}
---
+++ UdemyCodingExercise/src/com/speedconverter/TeenNumberChecker.java
@@ -0,0 +1,22 @@
+package com.speedconverter;
+
+public class TeenNumberChecker {
+
+	public static boolean hasTeen(int number1, int number2, int number3) {
+		
+		if (isTeen(number1) || isTeen(number2) || isTeen(number3)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	private static boolean isTeen(int number) {
+		
+		if (number >= 13 && number <= 19) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
